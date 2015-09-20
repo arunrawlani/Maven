35c3068635c9aa3c5c629adad3669a774f873f7e
Arun Rawlani <arun.rawlani@mail.mcgill.ca>
2015-09-20 10:43:14 -0400

    Commit

--- Sherpa/InfluencerViewController.swift
+++ Sherpa/InfluencerViewController.swift
@@ -13,6 +13,7 @@ import PNChartSwift
 
 class InfluencerViewController: UIViewController{
     
+@IBOutlet weak var riskButton: UIButton!
     override func viewDidLoad() {
         let reactionTimeData = BarGraph(frame: CGRectMake(20, -80, 330, 200))
         self.view.addSubview(reactionTimeData)
@@ -20,6 +21,43 @@ class InfluencerViewController: UIViewController{
     
     @IBAction func computeIndex(sender: AnyObject) {
         
-        performSegueWithIdentifier("compute", sender: nil)
+        riskButton.hidden = true
+        self.demoSpinner()
+    }
+    
+    func delay(#seconds: Double, completion:()->()) {
+        let popTime = dispatch_time(DISPATCH_TIME_NOW, Int64( Double(NSEC_PER_SEC) * seconds ))
+        
+        dispatch_after(popTime, dispatch_get_main_queue()) {
+            completion()
+        }
+    }
+    
+    func demoSpinner() {
+        
+        SwiftSpinner.showWithDelay(2.0, title: "It's taking longer than expected")
+        
+        delay(seconds: 0.0, completion: {
+            SwiftSpinner.show("Computing Social Index")
+        })
+        
+        delay(seconds: 3.0, completion: {
+            SwiftSpinner.show("Fetching session data")
+        })
+        
+        delay(seconds: 5.0, completion: {
+            SwiftSpinner.show("Analyzing trends", animated: false)
+        })
+        
+        delay(seconds: 6.0, completion: {
+            SwiftSpinner.setTitleFont(UIFont(name: "Futura", size: 22.0))
+            SwiftSpinner.show("Computed")
+        })
+        
+        delay(seconds: 7.0, completion: {
+            SwiftSpinner.hide()
+            self.performSegueWithIdentifier("compute", sender: nil)
+        })
+   
     }
 }
